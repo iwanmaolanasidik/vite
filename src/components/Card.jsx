95b3951073443d80b60843fa1c55a0a966c1336b
iwanmaolanasidik <iwanmaolanasidik7@gmail.com>
2023-07-23 23:40:08 +0700

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function Card() {
+    const [count, setCount] = useState(0);
+    return (
+        <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
+            <p>
+                Edit <code>src/App.jsx</code> and save to test Berewek
+            </p>
+        </div>
+    );
+}
+
+export default Card;
